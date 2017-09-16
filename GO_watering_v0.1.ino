7705de7b32ae16836867ae864860db58281e3e20
theJobbitt <nick@thejobbitt.com>
2017-09-16 01:55:26 -0400

    V0.1

---
+++ GO_watering_v0.1.ino
@@ -0,0 +1,18 @@
+// GrowOpen Water System
+// v0.1
+
+#include <Time.h>
+#include <TimeLib.h>
+
+//-OUTPUT PINS---------------------------------
+const uint8_t WATER_INLET_SOLENOID = 1;
+const uint8_t WATER_TRANS_SOLENOID = 2;
+const uint8_t MIXER_1 = 3;
+const uint8_t MIXER_2 = 4;
+
+//-INPUT PINS----------------------------------
+const uint8_t VAT_1_TOP = 5;
+const uint8_t VAT_1_BOT = 6;
+const uint8_t VAT_2_TOP = 7;
+const uint8_t VAT_2_BOT = 8;
+
